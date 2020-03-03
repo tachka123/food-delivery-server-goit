2724b6b7b634d10f47fc66688cd1a37e5e887cb0
Alexander <123tachka123@gmail.com>
2020-03-04 00:51:12 +0200

    added npm start

--- src/requestHandlers/requestHandler.js
+++ src/requestHandlers/requestHandler.js
@@ -6,7 +6,7 @@ const logger = morgan('combined');
 const mainHandler = (req, res) => {
 	const { url } = req;
 	const func = routes[url] || routes.defaultRoute;
-	logger(req, res, () => func(req, res));
+	logger(req, res, (req, res) => func(req, res));
 };
 
 module.exports = mainHandler;
