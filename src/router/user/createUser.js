7052e86f4eff180d60ad47099d4b9a2d2775d9ff
Alexander <123tachka123@gmail.com>
2020-03-14 23:44:57 +0200

    excess deleted

--- src/router/user/createUser.js
+++ src/router/user/createUser.js
@@ -7,28 +7,34 @@ const result = { status: 'success', user: '' };
 
 const createUser = (req, res) => {
 	const { username, telephone, password, email } = req.body;
-	if ((username, telephone, password, email)) {
-		const user = { username, telephone, password, email, id: uuid.v1() };
-		fs.readFile(usersPath, (err, data) => {
-			if (err) throw err;
-			const arr = JSON.parse(data);
-			const checkForAlreadeCreatedUser = arr.find(item => item.username === username);
-			if (!checkForAlreadeCreatedUser) {
-				const newArray = [...arr, user];
-				fs.writeFile(usersPath, JSON.stringify(newArray), err => {
-					if (err) throw err;
-					res.type('json');
-					result.user = username;
-					res.send(result);
+	try {
+		if ((username, telephone, password, email)) {
+			const user = { username, telephone, password, email, id: uuid.v1() };
+			fs.readFile(usersPath, (err, data) => {
+				if (err) throw err;
+				const arr = JSON.parse(data);
+				const checkForAlreadeCreatedUser = arr.find(item => item.username === username);
+				if (!checkForAlreadeCreatedUser) {
+					const newArray = [...arr, user];
+					fs.writeFile(usersPath, JSON.stringify(newArray), err => {
+						if (err) throw err;
+						res.type('json');
+						result.user = username;
+						res.send(result);
+						res.end();
+					});
+				} else {
+					res.send('User with same nickname is created already');
 					res.end();
-				});
-			} else {
-				res.send('User with same nickname is created already');
-				res.end();
-			}
-		});
-	} else {
-		res.status(400).send('Some of field is empty');
+				}
+			});
+		} else {
+			res.status(400).send('Some of field is empty');
+			res.end();
+		}
+	} catch {
+		res.status(500);
+		res.send('Server error');
 		res.end();
 	}
 };
--- src/routes/default/default.js
+++
@@ -1,7 +0,0 @@
-const defaultRoute = (req, res) => {
-	res.writeHead(400);
-	res.write('Bad request');
-	res.end();
-};
-
-module.exports = defaultRoute;
